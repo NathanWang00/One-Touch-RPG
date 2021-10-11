e4f397d38ccf3f0c94306c09c262889f87b80641
Nathan Wang <nxwang@ucsc.edu>
2021-10-10 20:05:27 -0700

    Inputs

--- docs/onetouchrpg/main.js
+++ docs/onetouchrpg/main.js
@@ -9,7 +9,13 @@ characters = [];
 
 const G = {
 	WIDTH: 100,
-	HEIGHT: 120
+	HEIGHT: 120,
+
+	GUARD_HOLD_LENGTH: 0.3,
+	RESET_LENGTH: 0.15,
+
+	STAB_TAP_AMOUNT: 2,
+	STAB_DELAY: 0.1,
 }
 
 options = {
@@ -33,20 +39,80 @@ const playerStates = {
  */
 let playerState = playerStates.DEFAULT;
 
+/**
+ * @type { number }
+ */
+ let pressedTime = 0;
+
+/**
+ * @type { number }
+ */
+ let releasedTime = 0;
+
+ /**
+ * @type { number }
+ */
+  let lastStabTime = 0;
+
+ /**
+ * @type { number }
+ */
+let tapAmount = 0;
+
 function update() {
 	if (!ticks) {
 
 	}
+	// input and state determination
+	if (input.isJustPressed) {
+		releasedTime = 0;
+	}
+	if (input.isJustReleased) {
+		if (playerState == playerStates.GUARDING) {
+			playerState = playerStates.DEFAULT;
+		} else if (pressedTime < G.GUARD_HOLD_LENGTH) {
+			tapAmount++;
+			if (tapAmount >= G.STAB_TAP_AMOUNT) {
+				playerState = playerStates.STABBING;
+			}
+		}
+		pressedTime = 0;
+	}
+	if (input.isPressed) {
+		pressedTime += 1/60; // all inputs are measured in seconds
+		if (pressedTime >= G.GUARD_HOLD_LENGTH) {
+			playerState = playerStates.GUARDING;
+			tapAmount = 0;
+		}
+	} else {
+		releasedTime += 1/60;
+		if (releasedTime >= G.RESET_LENGTH) {
+			if (tapAmount >= G.STAB_TAP_AMOUNT) {
+				playerState = playerStates.DEFAULT;
+			} else if (tapAmount > 0) {
+				playerState = playerStates.SLASHING;
+			}
+			tapAmount = 0;
+		}
+	}
 
+	// player actions
 	switch (playerState) {
 		case playerStates.DEFAULT:
 			break;
 		case playerStates.GUARDING:
+			console.log("guarding");
 			break;
 		case playerStates.SLASHING:
+			console.log("slash");
+			playerState = playerStates.DEFAULT;
 			break;
 		case playerStates.STABBING:
+			lastStabTime -= 1/60;
+			if (lastStabTime <= 0) {
+				lastStabTime = G.STAB_DELAY;
+				console.log("stab");
+			}
 			break;
 	}
-	console.log(playerState);
 }
